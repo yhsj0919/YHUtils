86048935ff2b6c7903be43b874b3d88a39a7ae58
永恒瞬间 <1130402124@qq.com>
2015-05-19 17:15:14 +0800

    1.1.2
    添加ui框架

---
+++ YHUI/app/src/main/java/com/yh/yhui/app/fragment/MainFragment.java
@@ -0,0 +1,34 @@
+package com.yh.yhui.app.fragment;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import com.yh.yhui.app.R;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class MainFragment extends Fragment {
+
+
+    public MainFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+
+        return rootView;
+    }
+
+
+}
