c3eff9c38a7e7648bc0b10ab435b9a532a1fd81a
LOVE <1130402124@qq.com>
2015-05-21 00:01:31 +0800

    1.1.2.2
    还是在测试

--- YHUI/app/src/main/java/com/yh/yhui/app/activity/MainActivity.java
+++ YHUI/app/src/main/java/com/yh/yhui/app/activity/MainActivity.java
@@ -1,7 +1,11 @@
 package com.yh.yhui.app.activity;
 
+import android.graphics.Bitmap;
 import android.graphics.Color;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
@@ -21,200 +25,216 @@ import com.mikepenz.materialdrawer.model.interfaces.Nameable;
 import com.mikepenz.materialdrawer.util.KeyboardUtil;
 import com.yh.yhui.app.R;
 import com.yh.yhui.app.fragment.MainFragment;
-
+import com.yh.yhui.app.fragment.SecondFragment;
 import io.codetail.animation.SupportAnimator;
 import io.codetail.animation.ViewAnimationUtils;
+import xyz.yhsj.yhutils.tools.phone.ScreenUtils;
 
 
 public class MainActivity extends AppCompatActivity {
 
-    //save our header or result
-    private Drawer.Result result = null;
+	View myView;
+	//save our header or result
+	private Drawer.Result result = null;
+	private Toolbar toolbar;
+	private AccountHeader.Result headerResult = null;
 
-    private Toolbar toolbar;
+	SecondFragment secondFragment;
 
-    private AccountHeader.Result headerResult = null;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
 
+		// Handle Toolbar
+		toolbar = (Toolbar) findViewById(R.id.toolbar);
+		setSupportActionBar(toolbar);
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+		myView = findViewById(R.id.content_root);
 
-        // Handle Toolbar
-        toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+		secondFragment=new SecondFragment();
+		setDrawer(savedInstanceState);
+	}
 
-        setDrawer(savedInstanceState);
-    }
+	private void setDrawer(Bundle savedInstanceState) {
 
-    private void setDrawer(Bundle savedInstanceState) {
+		headerResult = new AccountHeader()
+				.withActivity(this)
+				.withHeaderBackground(R.drawable.header)
+				.addProfiles(
+						new ProfileDrawerItem().withName("永恒瞬间").withEmail("1130402124@qq.com").withIcon(getResources().getDrawable(R.drawable.profile)),
+						new ProfileSettingDrawerItem().withName("修改密码").withDescription("修改密码").withIcon(GoogleMaterial.Icon.gmd_settings_backup_restore).withIdentifier(1),
+						new ProfileSettingDrawerItem().withName("管理账户").withIcon(GoogleMaterial.Icon.gmd_settings).withIdentifier(2)
+				)
 
-        headerResult = new AccountHeader()
-                .withActivity(this)
-                .withHeaderBackground(R.drawable.header)
-                .addProfiles(
-                        new ProfileDrawerItem().withName("永恒瞬间").withEmail("1130402124@qq.com").withIcon(getResources().getDrawable(R.drawable.profile)),
-                        new ProfileSettingDrawerItem().withName("修改密码").withDescription("修改密码").withIcon(GoogleMaterial.Icon.gmd_settings_backup_restore).withIdentifier(1),
-                        new ProfileSettingDrawerItem().withName("管理账户").withIcon(GoogleMaterial.Icon.gmd_settings).withIdentifier(2)
-                )
+				.withOnAccountHeaderListener(new AccountHeader.OnAccountHeaderListener() {
+					@Override
+					public boolean onProfileChanged(View view, IProfile profile, boolean current) {
 
-                .withOnAccountHeaderListener(new AccountHeader.OnAccountHeaderListener() {
-                    @Override
-                    public boolean onProfileChanged(View view, IProfile profile, boolean current) {
+						if (profile != null && profile instanceof IDrawerItem) {
+							switch (profile.getIdentifier()) {
+								case 1:
+									Toast.makeText(MainActivity.this, "修改密码", Toast.LENGTH_SHORT).show();
+									break;
+								case 2:
+									Toast.makeText(MainActivity.this, "设置", Toast.LENGTH_SHORT).show();
+									break;
+								default:
+									break;
+							}
+						}
 
-                        if (profile != null && profile instanceof IDrawerItem) {
-                            switch (profile.getIdentifier()) {
-                                case 1:
-                                    Toast.makeText(MainActivity.this, "修改密码", Toast.LENGTH_SHORT).show();
-                                    break;
-                                case 2:
-                                    Toast.makeText(MainActivity.this, "设置", Toast.LENGTH_SHORT).show();
-                                    break;
-                                default:
-                                    break;
-                            }
-                        }
+						return false;
+					}
+				})
+				.withSavedInstance(savedInstanceState)
+				.build();
 
-                        return false;
-                    }
-                })
-                .withSavedInstance(savedInstanceState)
-                .build();
+		result = new Drawer()
+				.withActivity(this)
+				.withToolbar(toolbar)
+				.withActionBarDrawerToggle(true)
+				.withAccountHeader(headerResult)
+				.addDrawerItems(
+						new PrimaryDrawerItem().withName("待办").withIdentifier(1).withBadge("5").withIcon(GoogleMaterial.Icon.gmd_access_alarm),
+						new SectionDrawerItem().withName("分割线"),
 
-        result = new Drawer()
-                .withActivity(this)
-                .withToolbar(toolbar)
-                .withActionBarDrawerToggle(true)
-                .withAccountHeader(headerResult)
-                .addDrawerItems(
-                        new PrimaryDrawerItem().withName("待办").withIdentifier(1).withBadge("5").withIcon(GoogleMaterial.Icon.gmd_access_alarm),
-                        new SectionDrawerItem().withName("分割线"),
+						new PrimaryDrawerItem().withName("主页").withIdentifier(2).withIcon(GoogleMaterial.Icon.gmd_home),
+						new SecondaryDrawerItem().withName("hello").withIcon(GoogleMaterial.Icon.gmd_home),
+						new DividerDrawerItem(),
+						new PrimaryDrawerItem().withName("设置").withIcon(GoogleMaterial.Icon.gmd_settings).withIdentifier(9)
+				).addStickyDrawerItems(
+						new SecondaryDrawerItem().withName("测试").withIcon(FontAwesome.Icon.faw_cog).withIdentifier(10)
+				)
+				.withOnDrawerItemClickListener(new Drawer.OnDrawerItemClickListener() {
+					@Override
+					public void onItemClick(AdapterView<?> parent, View view, int position, long id, final IDrawerItem drawerItem) {
+						if (drawerItem != null && drawerItem instanceof Nameable) {
 
-                        new PrimaryDrawerItem().withName("主页").withIdentifier(2).withIcon(GoogleMaterial.Icon.gmd_home),
-                        new SecondaryDrawerItem().withName("hello").withIcon(GoogleMaterial.Icon.gmd_home),
-                        new DividerDrawerItem(),
-                        new PrimaryDrawerItem().withName("设置").withIcon(GoogleMaterial.Icon.gmd_settings).withIdentifier(9)
-                ).addStickyDrawerItems(
-                        new SecondaryDrawerItem().withName("测试").withIcon(FontAwesome.Icon.faw_cog).withIdentifier(10)
-                )
-                .withOnDrawerItemClickListener(new Drawer.OnDrawerItemClickListener() {
-                    @Override
-                    public void onItemClick(AdapterView<?> parent, View view, int position, long id, IDrawerItem drawerItem) {
-                        if (drawerItem != null && drawerItem instanceof Nameable) {
+							switch (drawerItem.getIdentifier()) {
+								case 1:
+									getSupportActionBar().setTitle(((Nameable) drawerItem).getName());
+									//getSupportActionBar().setSubtitle("fu标题");
+									getSupportFragmentManager().beginTransaction().replace(R.id.content_frame, new MainFragment()).commit();
+									break;
+								case 2:
 
-                            switch (drawerItem.getIdentifier()) {
-                                case 1:
-                                    getSupportActionBar().setTitle(((Nameable) drawerItem).getName());
-                                    //getSupportActionBar().setSubtitle("fu标题");
-                                    getSupportFragmentManager().beginTransaction().replace(R.id.content_frame, new MainFragment()).commit();
-                                    break;
-                                case 2:
-                                    getSupportActionBar().setTitle(((Nameable) drawerItem).getName());
-                                    //getSupportActionBar().setSubtitle("fu标题2");
-
-                                    View myView = findViewById(R.id.content_root);
-
-                                    // get the center for the clipping circle
-                                    int cx = (myView.getLeft() + myView.getRight()) / 2;
-                                    int cy = (myView.getTop() + myView.getBottom()) / 2;
-
-                                    // get the final radius for the clipping circle
-                                    int finalRadius = Math.max(myView.getWidth(), myView.getHeight());
-
-                                    SupportAnimator animator =ViewAnimationUtils.createCircularReveal(myView, 30, cy, 0, finalRadius);
-
-                                    animator.setInterpolator(new AccelerateInterpolator());
-
-                                    animator.setDuration(500);
-
-                                    findViewById(R.id.content_overlay).setBackgroundColor(Color.parseColor("#666666"));
-
-                                    animator.start();
-
-                                    getSupportFragmentManager().beginTransaction().replace(R.id.content_frame, new MainFragment()).commit();
-                                    break;
-                                default:
-                                    break;
-
-                            }
-
-                        }
-                    }
-                })
-                .withOnDrawerListener(new Drawer.OnDrawerListener() {
-                    @Override
-                    public void onDrawerOpened(View drawerView) {
-                        KeyboardUtil.hideKeyboard(MainActivity.this);
-                    }
-
-                    @Override
-                    public void onDrawerClosed(View drawerView) {
-
-                    }
-
-                    @Override
-                    public void onDrawerSlide(View drawerView, float slideOffset) {
-
-                    }
-                })
-                .withFireOnInitialOnClick(true)
-                .withSavedInstance(savedInstanceState)
-                .build();
-
-        result.keyboardSupportEnabled(this, true);
+									//getSupportActionBar().setSubtitle("fu标题2");
+//									new Handler().postDelayed(new Runnable() {
+//										public void run() {
+											getSupportActionBar().setTitle(((Nameable) drawerItem).getName());
+											chengeAty(secondFragment, null);
+//										}
+//									}, 500);
 
 
-    }
+									break;
+								default:
+									break;
+
+							}
+
+						}
+					}
+				})
+				.withOnDrawerListener(new Drawer.OnDrawerListener() {
+					@Override
+					public void onDrawerOpened(View drawerView) {
+						KeyboardUtil.hideKeyboard(MainActivity.this);
+					}
+
+					@Override
+					public void onDrawerClosed(View drawerView) {
+
+					}
+
+					@Override
+					public void onDrawerSlide(View drawerView, float slideOffset) {
+
+					}
+				})
+				.withFireOnInitialOnClick(true)
+				.withSavedInstance(savedInstanceState)
+				.build();
+
+		result.keyboardSupportEnabled(this, true);
 
 
-    @Override
-    protected void onSaveInstanceState(Bundle outState) {
-        //add the values which need to be saved from the drawer to the bundle
-        outState = result.saveInstanceState(outState);
-        super.onSaveInstanceState(outState);
-    }
+	}
 
-    @Override
-    public void onBackPressed() {
-        //handle the back press :D close the drawer first and if the drawer is closed close the activity
-        if (result != null && result.isDrawerOpen()) {
-            result.closeDrawer();
-        } else {
-            super.onBackPressed();
-        }
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+	private void chengeAty(Fragment myfragment, Bitmap ScreenBitmap) {
 
 
-        switch (item.getItemId()) {
+		// get the center for the clipping circle
+		int cx = (myView.getLeft() + myView.getRight()) / 2;
+		int cy = (myView.getTop() + myView.getBottom()) / 2;
 
-            case R.id.action_chenge:
-                System.err.println(item.getTitle());
-                return true;
-            case R.id.action_add:
-                System.err.println(item.getTitle());
-                return true;
-            case R.id.action_settings:
-                System.err.println(item.getTitle());
-                return true;
+		// get the final radius for the clipping circle
+		int finalRadius = Math.max(myView.getWidth(), myView.getHeight());
 
-        }
-        return super.onOptionsItemSelected(item);
-    }
+		SupportAnimator animator = ViewAnimationUtils.createCircularReveal(myView, 20, cy, 0, finalRadius);
+
+		animator.setInterpolator(new AccelerateInterpolator());
+
+		animator.setDuration(1500);
+
+		findViewById(R.id.content_overlay).setBackgroundColor(Color.BLUE);
+//		findViewById(R.id.content_overlay).setBackgroundDrawable(new BitmapDrawable(getResources(), ScreenBitmap));
+
+		animator.start();
+
+		getSupportFragmentManager().beginTransaction().replace(R.id.content_frame, myfragment).commit();
+
+	}
+
+
+	@Override
+	protected void onSaveInstanceState(Bundle outState) {
+		//add the values which need to be saved from the drawer to the bundle
+		outState = result.saveInstanceState(outState);
+		super.onSaveInstanceState(outState);
+	}
+
+	@Override
+	public void onBackPressed() {
+		//handle the back press :D close the drawer first and if the drawer is closed close the activity
+		if (result != null && result.isDrawerOpen()) {
+			result.closeDrawer();
+		} else {
+			super.onBackPressed();
+		}
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.menu_main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+
+
+		switch (item.getItemId()) {
+
+			case R.id.action_chenge:
+				System.err.println(item.getTitle());
+				return true;
+			case R.id.action_add:
+				System.err.println(item.getTitle());
+				return true;
+			case R.id.action_settings:
+				System.err.println(item.getTitle());
+				return true;
+
+		}
+		return super.onOptionsItemSelected(item);
+	}
 
 
 }
---
+++ YHUI/app/src/main/java/com/yh/yhui/app/fragment/SecondFragment.java
@@ -0,0 +1,32 @@
+package com.yh.yhui.app.fragment;
+
+
+import android.os.Bundle;
+
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.yh.yhui.app.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class SecondFragment extends Fragment {
+
+
+	public SecondFragment() {
+		// Required empty public constructor
+	}
+
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+	                         Bundle savedInstanceState) {
+		// Inflate the layout for this fragment
+		return inflater.inflate(R.layout.fragment_second, container, false);
+	}
+
+
+}
